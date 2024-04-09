be22d53ea149a609f6a8995b5ce755c88f3d5cef
YuDongGeun <fkdlem8343@naver.com>
2024-04-09 15:15:52 +0900

    10808 알파벳 개수 수정

--- algorithm/10808.cpp
+++ algorithm/10808.cpp
@@ -1,21 +1,16 @@
-/*
-1. 문자열 입력
-2. 문자열 길이만큼 반복하면서 알파벳에 해당하는 배열을 카운팅한다. 
-3. 배열을 순회하면서 출력한다. 
-*/ 
-
 #include <bits/stdc++.h>
 using namespace std; 
 
 int main(){
 	string s;
-	int a[26] = { 0 };
+	int cnt[26] = { 0 };
 	cin >> s;
-	for(int i = 0; i < s.size(); i++){
-		a[s[i] - 'a']++;
+	for(char a : s){
+		cnt[a - 'a']++;	
 	}
+		
 	for(int i = 0; i < 26; i++){
-		cout << a[i] << " ";
+		cout << cnt[i] << " ";
 	}
 	return 0;
 }
